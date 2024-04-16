6811480a63310f1fbb7974eacfbcf36429b26bd8
Cr1stopherB <cristopherbravo13@gmail.com>
2024-04-16 18:49:16 -0400

    Cambios nuevos

--- ProgramasPython/1.-PrimerEjemplo.py
+++ ProgramasPython/1.-PrimerEjemplo.py
@@ -1,5 +1,8 @@
 # Solicitar al usuario que ingrese su nombre
 nombre = input("Por favor, ingresa tu nombre: ")
-
+num1 = input("Ingrese un numero entero:")
+num2 = input("Ingrese un numero decimal:")
 # Imprimir un saludo personalizado
 print("Hola,", nombre, "! Bienvenido a Python.")
+print("El numero que usted eligio es:", num1)
+print("El numero decimal que usted eligio es:", num2)
