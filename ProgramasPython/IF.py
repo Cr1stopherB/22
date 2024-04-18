a6f90306f6ebdf5ccfc6634897345809e04822c4
Cr1stopherB <cristopherbravo13@gmail.com>
2024-04-17 20:02:17 -0400

    17-4-24

---
+++ ProgramasPython/IF.py
@@ -0,0 +1,53 @@
+# tacos=1
+# pizza=2
+# humita=3
+# cazuela=4
+
+# print("Elija una opcion de comida: 1.-Tacos, 2.-Pizza, 3.-Humita, 4.-Cazuela")
+# op=int(input())
+
+# if op==(1):
+#     print("Usted prefiere los Tacos")
+# elif op==(2):
+#      print("Usted prefiere pizza")
+# elif op==(3):
+#      print("Usted prefiere humita")
+# elif op==(4):
+#      print("Usted prefiere cazuela")
+# else:
+#      print("Usted no prefiere nada")
+
+# boleana=False
+# if boleana:
+#     print("Este bloque se va a ejecutar")
+
+##EJEMPLO DE VARIABLE BOLEANA
+# ticket_valido=False
+
+# folio_valido=123
+
+# n_folio=print("Ingrese el numero de folio")
+# n_folio=int(input)
+
+
+# if n_folio==folio_valido:
+#     ticket_valido=True
+
+    
+# if ticket_valido:
+#     print("Este ticket es valido")
+# else:
+#     print("Este ticket caducó")
+
+edad=18
+grupo="A"
+
+if edad>=18 and grupo=="A":
+    print("Usted es mayor y pertenece al grupo A")
+elif edad>18 and grupo=="A":
+    print("Usted es mayor y pertenece al grupo B")
+else:
+    print("Usted no es mayor y no pertenece a ningun grupo")
+
+
+
---
+++ ProgramasPython/LISTAS.py
@@ -0,0 +1,6 @@
+lista_numeros = [1, 2, 3, 4, 5]
+
+
+for valor in lista_numeros:
+    if valor>3:
+        print("El numero es ",valor, " es mayor que 3")
