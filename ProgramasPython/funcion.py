a8e608bf8f9a3d1031f8ecb95fdb3eeeb567c961
Cr1stopherB <cristopherbravo13@gmail.com>
2024-04-18 20:11:36 -0400

    fun

---
+++ ProgramasPython/funcion.py
@@ -0,0 +1,46 @@
+def multiPorDos():
+    print("Ingrese un numero")
+    num=int(input())
+    print(num*2)
+
+multiPorDos()
+
+def resta5():
+    print("Ingrese un numero")
+    num=int(input())
+    print(num-5)
+
+resta5()
+
+def suma():
+    print("Ingrese el numero 1: ")
+    num1=int(input())
+    print("Ingrese el numero 2: ")
+    num2=int(input())
+    print(num1+num2)
+
+suma()
+
+
+def suma(num1, num2):
+    resultado=num1+num2
+    return resultado
+
+print(suma(2,4))
+
+
+def si(ancho, altura):
+    resultado=ancho*altura
+    print("ingrese el ancho del rectangulo")
+    ancho=int(input())
+    print("Ingrese la altura de el rectangulo")
+    altura=int(input())
+    return resultado
+
+
+#LLAMAR A LA FUNCION 
+
+
+
+#SIN ARGUMENTO () <-
+# <-CUANDO LA FUNCION NO TIEEN ARGUMENTO SE RABAJA DE ESTA FORMA
