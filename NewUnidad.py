96fb23fac7b1ff1edcc4046d6ac8719c871b32d1
Cr1stopherB <cristopherbravo13@gmail.com>
2024-05-16 20:13:24 -0400

    new

---
+++ NewUnidad.py
@@ -0,0 +1,29 @@
+# listan1= [11,22,33,44,55]
+# print(listan1[0]+listan1[0])
+
+# lista=[1,2,3,4,5,6]
+
+# for i in lista:
+#     print(lista)
+
+
+# lista2= [45,90,23,45,67,87]
+# lista2.append(89)
+# import random
+
+# num1=random.randint(0,100)
+# num2=random.randint(0,100)
+
+# listaTarea=[10,20,30,40,50]
+# listaTarea.append(num1)
+# listaTarea.append(num2)
+
+# print(listaTarea)
+
+
+lista2= [90,67,64,90,23]
+comodin=10
+lista2.append(comodin)
+for i in lista2:
+    print(i)
+
