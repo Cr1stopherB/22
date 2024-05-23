36ffa7d5d888ed3b903b801b8fc14a50240d6541
Cr1stopherB <cristopherbravo13@gmail.com>
2024-05-22 20:06:59 -0400

    lo

---
+++ Unidad3.py/22-05.py
@@ -0,0 +1,39 @@
+# lista1=["Pedro", "juan", "diego", 10, 20 , 30]
+# lista1.append("caca")
+# print(lista1)
+
+# for i in lista1:
+#     print(i)
+# RECORRE LISTA
+    
+
+# lista=["Chris", "Bravo"]
+# lista.append("Alonso")
+# lista.append("Flores")
+
+# for i in lista:
+#     print(i)
+
+
+# lista2=[22, 45, 55, 444, 66, 33]
+
+# # lista2.insert(0, "Chris")
+# # lista2.insert(3, "Bravo")
+# lista2.append(18)      #No es necesario poner indice 8, se remplaza por APPEND porque siempre va alfinal 
+# lista2.remove(22)
+# print(lista2)
+
+
+
+
+#Insert sirve para insertar algun valor a la lista, se introduce primero en que indice se desea insertar
+#Remove sacar elemento de la lista
+#Sort ordena de menor a menor
+
+
+lista2=[22, 45, 55, 444, 66, 33]
+lista2.sort()
+print(lista2)
+
+
+
