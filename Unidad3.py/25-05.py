5a73356d7751824a8b314f94c503ec54e3b52c76
Cr1stopherB <cristopherbravo13@gmail.com>
2024-05-25 14:15:53 -0400

    ñ

---
+++ Unidad3.py/25-05.py
@@ -0,0 +1,10 @@
+import random
+
+nombres=["Chris", "Maximiliano", "Alberto", ""]
+edades=[]
+for i in range(len(nombres)):
+    num=random.randint(1, 99)
+    edades.append(num)
+
+for e in range(len(nombres)):
+    print("Su nombre es:", nombres[e], " y su edad es: ", str(edades[e]))
