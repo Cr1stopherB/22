144022cb792e04035f59d275db5c38517a1322cb
Cr1stopherB <cristopherbravo13@gmail.com>
2024-05-30 20:06:38 -0400

    p

---
+++ Unidad3.py/PO.py
@@ -0,0 +1,16 @@
+dicc_compras={
+    "Frutas": {
+    "Manzana": 200, "Pera": 300, "Platano": 900,
+    },
+    "Verduras": {
+    "Zanahoria": 1000, "Lechuga": 600, "Brocoli": 900
+    }
+}
+
+
+# diccionario = {"nombre": "Cesar Huispe",
+# "fonos": [
+#     988778882,
+#     988877776,
+#     877666333],
+# "activo": True}
